73941b20db9aae655e38aaa0f29fccc0ba0805e4
rilpires <rilpires@gmail.com>
2023-07-13 12:48:25 -0300

    Updating Loki Appender with some general improvements

--- lib/src/remote/loki_appender.dart
+++ lib/src/remote/loki_appender.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:intl/intl.dart';
 import 'package:logging_appenders/src/internal/dummy_logger.dart';
+import 'package:logging_appenders/src/logrecord_formatter.dart';
 import 'package:logging_appenders/src/remote/base_remote_appender.dart';
 
 final _logger = DummyLogger('logging_appenders.loki_appender');
@@ -16,54 +17,45 @@ class LokiApiAppender extends BaseDioLogSender {
     required this.username,
     required this.password,
     required this.labels,
-  })  : labelsString =
-            '{${labels.entries.map((entry) => '${entry.key}="${entry.value}"').join(',')}}',
-        authHeader = 'Basic ${base64.encode(utf8.encode([
-          username,
-          password
-        ].join(':')))}';
+    LogRecordFormatter? formatter,
+  }) : super(formatter: formatter);
 
   final String server;
   final String username;
   final String password;
-  final String authHeader;
-  final Map<String, String> labels;
-  final String labelsString;
+  Map<String, String> labels;
 
-  static final DateFormat _dateFormat =
-      DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+  String get _authHeader =>
+      'Basic ${base64.encode(utf8.encode([username, password].join(':')))}';
+
 
   Dio? _clientInstance;
 
   Dio get _client => _clientInstance ??= Dio();
 
-  static String _encodeLineLabelValue(String value) {
-    if (value.contains(' ')) {
-      return json.encode(value);
+  ///
+  ///
+  /// It creates a stream entry for each log level
+  List<LokiStream> lokiStreamsFromLogEntries(List<LogEntry> entries) {
+    Map<String, List<LogEntry>> entriesByLevel = {};
+
+    for (final entry in entries) {
+      final level = entry.lineLabels['lvl'] ?? '';
+      entriesByLevel.putIfAbsent(level, () => <LogEntry>[]);
+      entriesByLevel[level]!.add(entry);
     }
-    return value;
+
+    return entriesByLevel.entries.map((entry) {
+      final labels = Map<String, String>.from(this.labels);
+      if (entry.key != '') labels['level'] = entry.key;
+      return LokiStream(labels, entry.value);
+    }).toList(growable: false);
   }
 
   @override
   Future<void> sendLogEventsWithDio(List<LogEntry> entries,
       Map<String, String> userProperties, CancelToken cancelToken) {
-    final jsonObject =
-        LokiPushBody([LokiStream(labelsString, entries)]).toJson();
-    final jsonBody = json.encode(jsonObject, toEncodable: (dynamic obj) {
-      if (obj is LogEntry) {
-        return {
-          'ts': _dateFormat.format(obj.ts.toUtc()),
-          'line': [
-            obj.lineLabels.entries
-                .map((entry) =>
-                    '${entry.key}=${_encodeLineLabelValue(entry.value)}')
-                .join(' '),
-            obj.line,
-          ].join(' - ')
-        };
-      }
-      return obj.toJson();
-    });
+    final jsonBody = LokiPushBody(lokiStreamsFromLogEntries(entries)).toJson();
     return _client
         .post<dynamic>(
           'https://$server/api/prom/push',
@@ -71,7 +63,7 @@ class LokiApiAppender extends BaseDioLogSender {
           data: jsonBody,
           options: Options(
             headers: <String, String>{
-              HttpHeaders.authorizationHeader: authHeader,
+              HttpHeaders.authorizationHeader: _authHeader,
             },
             contentType: ContentType(
                     ContentType.json.primaryType, ContentType.json.subType)
@@ -80,7 +72,6 @@ class LokiApiAppender extends BaseDioLogSender {
         )
         .then(
           (response) => Future<void>.value(null),
-//      _logger.finest('sent logs.');
         )
         .catchError((Object err, StackTrace stackTrace) {
       String? message;
@@ -110,9 +101,30 @@ class LokiPushBody {
 class LokiStream {
   LokiStream(this.labels, this.entries);
 
-  final String labels;
+  final Map<String, String> labels;
   final List<LogEntry> entries;
 
+  String toLabelString(Map<String, String> labels) =>
+      '{${labels.entries.map((entry) => '${entry.key}="${entry.value}"').join(',')}}';
+
+  static final DateFormat _dateFormat =
+      DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+  static String _encodeLineLabelValue(String value) {
+    if (value.contains(' ')) {
+      return json.encode(value);
+    }
+    return value;
+  }
+
   Map<String, dynamic> toJson() =>
-      <String, dynamic>{'labels': labels, 'entries': entries};
+      <String, dynamic>{
+        'labels': toLabelString(labels),
+        'entries': entries
+            .map((LogEntry entry) => <String, String>{
+                  'ts': _dateFormat.format(entry.ts.toUtc()),
+                  'line': entry.line
+                })
+            .toList(),
+      };
 }
